0d32432f4c05a6294f03532e7d7d94c10c436f81
C47D <carlos.santiago.diaz@gmail.com>
2021-06-13 15:06:22 -0500

    touch driver: Add LVGL v8 compatibility

--- lvgl_touch/touch_driver.c
+++ lvgl_touch/touch_driver.c
@@ -26,7 +26,11 @@ void touch_driver_init(void)
 #endif
 }
 
+#if LVGL_VERSION_MAJOR >= 8
+void touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data)
+#else
 bool touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data)
+#endif
 {
     bool res = false;
 
@@ -46,6 +50,10 @@ bool touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data)
     res = gt911_read(drv, data);
 #endif
 
+#if LVGL_VERSION_MAJOR >= 8
+    data->continue_reading = res;
+#else
     return res;
+#endif
 }
 
--- lvgl_touch/touch_driver.h
+++ lvgl_touch/touch_driver.h
@@ -44,7 +44,12 @@ extern "C" {
  * GLOBAL PROTOTYPES
  **********************/
 void touch_driver_init(void);
+
+#if LVGL_VERSION_MAJOR >= 8
+void touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
+#else
 bool touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
+#endif
 
 #ifdef __cplusplus
 } /* extern "C" */
