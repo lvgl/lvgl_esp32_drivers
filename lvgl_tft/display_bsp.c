6122d139de3ef280ccad39e9be4c8d7936622197
C47D <carlos.santiago.diaz@gmail.com>
2021-07-29 19:04:39 -0500

    display_bsp: Move IO initialization here

--- lvgl_tft/display_bsp.c
+++ lvgl_tft/display_bsp.c
@@ -6,6 +6,24 @@
 #include "sdkconfig.h"
 #include "driver/gpio.h"
 
+void display_bsp_init_io(void)
+{
+#ifdef CONFIG_LV_DISPLAY_USE_DC
+    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_DC);
+    gpio_set_direction(CONFIG_LV_DISP_PIN_DC, GPIO_MODE_OUTPUT);
+#endif
+
+#ifdef CONFIG_LV_DISP_USE_RST
+    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_RST);
+    gpio_set_direction(CONFIG_LV_DISP_PIN_RST, GPIO_MODE_OUTPUT);
+#endif
+
+#ifdef CONFIG_LV_DISP_PIN_BCKL
+    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_BCKL);
+    gpio_set_direction(CONFIG_LV_DISP_PIN_BCKL, GPIO_MODE_OUTPUT);
+#endif
+}
+
 void display_bsp_delay(uint32_t delay_ms)
 {
     vTaskDelay(pdMS_TO_TICKS(delay_ms));
--- lvgl_tft/display_bsp.h
+++ lvgl_tft/display_bsp.h
@@ -1,6 +1,7 @@
 #ifndef DISPLAY_BSP_H_
 #define DISPLAY_BSP_H_
 
+void display_bsp_init_io(void);
 void display_bsp_delay(uint32_t delay_ms);
 void display_bsp_backlight(uint8_t state);
 void display_bsp_gpio_dc(uint8_t state);
