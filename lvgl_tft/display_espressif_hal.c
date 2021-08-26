0ae3f76097a4ca8cb3d203722165a74487808326
C47D <carlos.santiago.diaz@gmail.com>
2021-08-26 18:51:35 -0500

    hal: Fix typo on function signatures

--- lvgl_tft/display_espressif_hal.c
+++ lvgl_tft/display_espressif_hal.c
@@ -6,32 +6,14 @@
 #include "sdkconfig.h"
 #include "driver/gpio.h"
 
-void display_bsp_init_io(void)
-{
-#ifdef CONFIG_LV_DISPLAY_USE_DC
-    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_DC);
-    gpio_set_direction(CONFIG_LV_DISP_PIN_DC, GPIO_MODE_OUTPUT);
-#endif
-
-#ifdef CONFIG_LV_DISP_USE_RST
-    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_RST);
-    gpio_set_direction(CONFIG_LV_DISP_PIN_RST, GPIO_MODE_OUTPUT);
-#endif
-
-#ifdef CONFIG_LV_DISP_PIN_BCKL
-    gpio_pad_select_gpio(CONFIG_LV_DISP_PIN_BCKL);
-    gpio_set_direction(CONFIG_LV_DISP_PIN_BCKL, GPIO_MODE_OUTPUT);
-#endif
-}
-
-void display_bsp_delay(lv_disp_drv_t *drv, uint32_t delay_ms)
+void display_hal_delay(lv_disp_drv_t *drv, uint32_t delay_ms)
 {
     (void) drv;
 
     vTaskDelay(pdMS_TO_TICKS(delay_ms));
 }
 
-void display_bsp_backlight(lv_disp_drv_t *drv, uint8_t state)
+void display_hal_backlight(lv_disp_drv_t *drv, uint8_t state)
 {
     (void) drv;
 
@@ -40,7 +22,7 @@ void display_bsp_backlight(lv_disp_drv_t *drv, uint8_t state)
 #endif
 }
 
-void display_bsp_gpio_dc(lv_disp_drv_t *drv, uint8_t state)
+void display_hal_gpio_dc(lv_disp_drv_t *drv, uint8_t state)
 {
     (void) drv;
 
@@ -49,7 +31,7 @@ void display_bsp_gpio_dc(lv_disp_drv_t *drv, uint8_t state)
 #endif
 }
 
-void display_bsp_gpio_rst(lv_disp_drv_t *drv, uint8_t state)
+void display_hal_gpio_rst(lv_disp_drv_t *drv, uint8_t state)
 {
     (void) drv;
 
