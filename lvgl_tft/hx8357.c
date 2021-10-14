83875cc6bd6f691bce0a2d15b909ab6329624ee2
C47D <carlos.santiago.diaz@gmail.com>
2021-10-13 23:21:19 -0500

    hx8357: Replace ESP_LOG with LVGL logging

--- lvgl_tft/hx8357.c
+++ lvgl_tft/hx8357.c
@@ -18,14 +18,13 @@
 #include "hx8357.h"
 #include "disp_spi.h"
 #include "driver/gpio.h"
-#include <esp_log.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 /*********************
  *      DEFINES
  *********************/
-#define TAG        "HX8357"
+#define TAG        "HX8357: "
 
 #define MADCTL_MY  0x80 ///< Bottom to top
 #define MADCTL_MX  0x40 ///< Right to left
@@ -174,7 +173,7 @@ void hx8357_init(void)
 	vTaskDelay(120 / portTICK_RATE_MS);
 #endif
 
-	ESP_LOGI(TAG, "Initialization.");
+	LV_LOG_INFO(TAG, "Initialization.");
 
 	//Send all the commands
 	const uint8_t *addr = (displayType == HX8357B) ? initb : initd;
