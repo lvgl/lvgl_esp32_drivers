4118ce53ec3db44e9913e807808fd2064b9d36dd
Carlos Diaz <carlos.santiago.diaz@gmail.com>
2021-12-30 11:00:58 -0600

    Update sh1107.h

--- lvgl_tft/sh1107.h
+++ lvgl_tft/sh1107.h
@@ -39,8 +39,8 @@ extern "C" {
 
 void sh1107_init(void);
 void sh1107_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map);
-void sh1107_rounder(struct _disp_drv_t * disp_drv, lv_area_t *area);
-void sh1107_set_px_cb(struct _disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+void sh1107_rounder(lv_disp_drv_t * disp_drv, lv_area_t *area);
+void sh1107_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color, lv_opa_t opa);
 void sh1107_sleep_in(void);
 void sh1107_sleep_out(void);
