ff7b3801f71fdeafbd3325d4c0506e6526628960
C47D <carlos.santiago.diaz@gmail.com>
2022-01-14 13:15:44 -0600

    Revert "esp_backlight: Replace gpio with esp_rom API"
    This reverts commit 24e4bf0b888e68f09bcf1b900afa41ef76db2371.

--- lvgl_tft/esp_lcd_backlight.c
+++ lvgl_tft/esp_lcd_backlight.c
@@ -12,9 +12,6 @@
 #include "esp_log.h"
 #include "soc/ledc_periph.h" // to invert LEDC output on IDF version < v4.3
 
-#include "esp_rom_gpio.h"
-#include "soc/gpio_sig_map.h"
-
 typedef struct {
     bool pwm_control; // true: LEDC is used, false: GPIO is used
     int index;        // Either GPIO or LEDC channel
@@ -59,15 +56,15 @@ disp_backlight_h disp_backlight_new(const disp_backlight_config_t *config)
 
         ESP_ERROR_CHECK(ledc_timer_config(&LCD_backlight_timer));
         ESP_ERROR_CHECK(ledc_channel_config(&LCD_backlight_channel));
-        esp_rom_gpio_connect_out_signal(config->gpio_num, ledc_periph_signal[LEDC_LOW_SPEED_MODE].sig_out0_idx + config->channel_idx, config->output_invert, 0);
+        gpio_matrix_out(config->gpio_num, ledc_periph_signal[LEDC_LOW_SPEED_MODE].sig_out0_idx + config->channel_idx, config->output_invert, 0);
     }
     else
     {
         // Configure GPIO for output
         bckl_dev->index = config->gpio_num;
-        esp_rom_gpio_pad_select_gpio(config->gpio_num);
+        gpio_pad_select_gpio(config->gpio_num);
         ESP_ERROR_CHECK(gpio_set_direction(config->gpio_num, GPIO_MODE_OUTPUT));
-        esp_rom_gpio_connect_out_signal(config->gpio_num, SIG_GPIO_OUT_IDX, config->output_invert, false);
+        gpio_matrix_out(config->gpio_num, SIG_GPIO_OUT_IDX, config->output_invert, false);
     }
 
     return (disp_backlight_h)bckl_dev;
