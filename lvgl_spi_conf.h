bd1d0fcda84eb2551ac35194ebe100df656a67c7
C47D <carlos.santiago.diaz@gmail.com>
2022-01-27 21:13:46 -0600

    Compilation error when SPI Host is not selected

--- lvgl_spi_conf.h
+++ lvgl_spi_conf.h
@@ -75,14 +75,15 @@ extern "C" {
 
 #define ENABLE_TOUCH_INPUT  CONFIG_LV_ENABLE_TOUCH
 
+#if defined (CONFIG_LV_TFT_DISPLAY_PROTOCOL_SPI)
 /* Display controller SPI host configuration */
 #if defined (CONFIG_LV_TFT_DISPLAY_SPI2_HOST)
 #define TFT_SPI_HOST SPI2_HOST
 #elif defined (CONFIG_LV_TFT_DISPLAY_SPI3_HOST)
 #define TFT_SPI_HOST SPI3_HOST
 #else
-/* FIXME: This is not expected to work, only a placeholder */
-#define TFT_SPI_HOST 0
+#error SPI host not defined
+#endif
 #endif
 
 /* Touch controller SPI host configuration */
