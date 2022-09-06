9cb0e8f5050bea821955a1d6309a9c8ca5861ffb
xuhailong11 <xuhailong3@jd.com>
2022-09-06 13:01:04 +0800

    st7735s add CONFIG_LV_TFT_DISPLAY_OFFSETS

--- lvgl_tft/st7735s.h
+++ lvgl_tft/st7735s.h
@@ -37,8 +37,10 @@ extern "C" {
 //      https://github.com/adafruit/Adafruit-ST7735-Library
 //
 #define ST7735_GREENTAB160x80 // For 160 x 80 display (BGR, inverted, 26 / 1 offset)
-#define COLSTART            26
-#define ROWSTART            1
+#if (CONFIG_LV_TFT_DISPLAY_OFFSETS)
+#define COLSTART  CONFIG_LV_TFT_DISPLAY_X_OFFSET
+#define ROWSTART  CONFIG_LV_TFT_DISPLAY_Y_OFFSET
+#endif
 
 // Delay between some initialisation commands
 #define TFT_INIT_DELAY      0x80
