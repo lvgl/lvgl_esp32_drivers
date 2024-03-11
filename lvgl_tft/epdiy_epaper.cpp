58d5febe70db18127551843c2ec440ef1d8bda77
Laurence Bank <laurencebank@Laurences-Air.home>
2024-03-11 10:21:25 +0000

    Updated for color output on Kaleido e-paper

--- lvgl_tft/epdiy_epaper.cpp
+++ lvgl_tft/epdiy_epaper.cpp
@@ -15,7 +15,8 @@ enum EpdDrawMode updateMode = MODE_DU;
 /* Display initialization routine */
 void epdiy_init(void)
 {
-    epd_init(&epd_board_v7, &ED097TC2, EPD_LUT_64K);
+//    epd_init(&epd_board_v7, &ED097TC2, EPD_LUT_64K);
+    epd_init(&epd_board_v7, &EC060KH3, EPD_LUT_64K);
   // Set VCOM for boards that allow to set this in software (in mV).
   // This will print an error if unsupported. In this case,
   // set VCOM using the hardware potentiometer and delete this line.
@@ -44,29 +45,45 @@ void buf_area_to_framebuffer(const lv_area_t *area, const uint8_t *image_data) {
 /* A copy from epd_copy_to_framebuffer with temporary lenght prediction */
 void buf_copy_to_framebuffer(EpdRect image_area, const uint8_t *image_data) {
   assert(framebuffer != NULL);
-
-  for (uint32_t i = 0; i < image_area.width * image_area.height; i++) {
-    uint8_t val = (i % 2) ? (image_data[i / 2] & 0xF0) >> 4 : image_data[i / 2] & 0x0F;
-    int xx = image_area.x + i % image_area.width;
-    if (xx < 0 || xx >= epd_width()) {
-      continue;
-    }
-    int yy = image_area.y + i / image_area.width;
-    if (yy < 0 || yy >= epd_height()) {
-      continue;
-    }
-    uint8_t *buf_ptr = &framebuffer[yy * epd_width() / 2 + xx / 2];
-    if (xx % 2) {
-      *buf_ptr = (*buf_ptr & 0x0F) | (val << 4);
-    } else {
-      *buf_ptr = (*buf_ptr & 0xF0) | val;
-    }
-  }
-}
+  int x, xx = image_area.x;
+  int y, yy = image_area.y;
+  int w = image_area.width;
+  int h = image_area.height;
+  const uint8_t ucCLRMask[3] = {0xc0,0x38,0x7};
+  uint8_t uc, *s, *d;
+// source data is one byte per pixel (RGB233)
+   for (y=yy; y<(yy + h); y++) {
+       int i = (xx+y) % 3; // which color filter is in use?
+       s = (uint8_t *)&image_data[(y-yy) * w];
+       d = &framebuffer[(y * epd_width() / 2) + (xx / 2)];
+       x = xx;
+       if (x & 1) {
+          uc = d[0] & 0xf0; // special case for odd starting pixel
+          if (s[0] & ucCLRMask[i])
+              uc |= 0xf;
+          i++;
+          s++;
+          *d++ = uc;
+          x++;
+          if (i >= 3) i-=3;
+       }
+       for (; x<(xx + w); x+=2) { // work 2 pixels at a time
+            uc = 0;
+            if (s[0] & ucCLRMask[i]) uc |= 0xf;
+            i++; if (i >= 3) i -= 3;
+            if (s[1] & ucCLRMask[i]) uc |= 0xf0;
+            i++; if (i >= 3) i -= 3;
+            *d++ = uc;
+            s += 2;
+       } // for x
+   } // for y
+} /* buf_copy_to_framebuffer() */
 
 /* Required by LVGL. Sends the color_map to the screen with a partial update  */
 void epdiy_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
 {
+static int x1=65535,y1=65535,x2=-1,y2=-1;
+
     ++flushcalls;
     uint16_t w = lv_area_get_width(area);
     uint16_t h = lv_area_get_height(area);
@@ -78,6 +95,12 @@ void epdiy_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_ma
         .height = h
     };
 
+    // capture the upper left and lower right corners
+    if (area->x1 < x1) x1 = area->x1;
+    if (area->y1 < y1) y1 = area->y1;
+    if (area->x2 > x2) x2 = area->x2;
+    if (area->y2 > y2) y2 = area->y2;
+
     uint8_t* buf = (uint8_t *) color_map;
     // Buffer debug
     /* for (int index=0; index<400; index++) {
@@ -88,12 +111,17 @@ void epdiy_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_ma
 
     //Faster mode suggested in LVGL forum (Leaves ghosting&prints bad sections / experimental) NOTE: Do NOT use in production
     //buf_area_to_framebuffer(area, buf);
-
-    epd_hl_update_area(&hl, updateMode, temperature, update_area); //update_area
-
+    if (lv_disp_flush_is_last(drv)) { // only send to e-paper when complete
+        update_area.x = x1;
+        update_area.y = y1;
+        update_area.width = (x2-x1)+1;
+        update_area.height = (y2-y1)+1;
+        epd_hl_update_area(&hl, updateMode, temperature, update_area); //update_area
+        x1 = y1 = 65535; x2 = y2 = -1; // reset update boundary
+    }
     //printf("epdiy_flush %d x:%d y:%d w:%d h:%d\n", flushcalls,(uint16_t)area->x1,(uint16_t)area->y1,w,h);
     /* Inform the graphics library that you are ready with the flushing */
-    lv_disp_flush_ready(drv);
+    lv_disp_flush_ready(drv); // do this after the check for "is_last"
 }
 
 /* 
@@ -103,17 +131,5 @@ void epdiy_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t* buf,
     lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color, lv_opa_t opa)
 {
-    // Test using RGB232
-    int16_t epd_color = 255;
-    if ((int16_t)color.full < 254) {
-        epd_color = (updateMode==MODE_DU) ? 0 : (int16_t)color.full / 3;
-    }
-
-    //Instead of using epd_draw_pixel: Set pixel directly in *buf that comes afterwards in flush as *color_map
-    uint16_t idx = (int16_t)y * buf_w / 2 + (int16_t)x / 2;
-    if (x % 2) {
-        buf[idx] = (buf[idx] & 0x0F) | (epd_color & 0xF0);
-    } else {
-        buf[idx] = (buf[idx] & 0xF0) | (epd_color >> 4);
-    }
+    buf[(y * buf_w)+x] = color.full;
 }
